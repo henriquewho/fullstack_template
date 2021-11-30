932d5be3953e4fb103804ac2e0dd213da99462f9
henriquewho <henriquegfonseca89@gmail.com>
2021-11-30 16:01:50 -0300

    simple change on index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
 const app = express(); 
 
+app.get("/", (request, response) => {
+    response.send('ok')
+})
+
 app.listen(3001, ()=>{
     console.log('Server on port 3001');
 })
 
 console.log('hello');
-
